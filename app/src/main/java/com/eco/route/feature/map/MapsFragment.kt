3221440ec0a5854a6153641d2992aa53a1a3edaf
7 <itismymessagebox@mail.ru>
2021-09-25 11:00:11 +0500

    bk commit

--- app/src/main/java/com/eco/route/feature/map/MapsFragment.kt
+++ app/src/main/java/com/eco/route/feature/map/MapsFragment.kt
@@ -1,12 +1,18 @@
 package com.eco.route.feature.map
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import com.eco.route.R
+import com.eco.route.feature.app.App
+import com.eco.route.feature.app.showToast
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -27,7 +33,34 @@ class MapsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        if (ActivityCompat.checkSelfPermission(
+                App.appContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                App.appContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ){
+            requestPermissionLauncher.launch(
+                Manifest.permission.ACCESS_FINE_LOCATION)
+        }else{
+            startMap()
+        }
+    }
+
+    private fun startMap(){
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(WorkMaps())
     }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                startMap()
+            } else {
+                showToast("Большинство функций не будут работать")
+            }
+        }
 }
