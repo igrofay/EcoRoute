6511f9599dbe33768d4c77f253b79f9ce9947ef5
nikitin.sa1265 <ss>
2021-09-26 09:47:06 +0300

    111

--- app/src/main/java/com/eco/route/feature/buildroute/BuildRouteFragment.kt
+++ app/src/main/java/com/eco/route/feature/buildroute/BuildRouteFragment.kt
@@ -5,8 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import com.eco.route.R
 
 
 class BuildRouteFragment : Fragment() {
@@ -17,13 +23,27 @@ class BuildRouteFragment : Fragment() {
     ): View? {
         return ComposeView(requireContext()).apply {
             setContent {
-
+                Column {
+                    BarRoute()
+                }
             }
         }
     }
 
     @Composable
-    fun BarRoute(){
+    fun BarRoute(str1: String , input1 : (String) -> Unit , str2: String , input2 : (String) -> Unit ){
+        Row {
+            Image(painter = painterResource(R.drawable.ic_back), contentDescription = "Назад")
+            Column(Modifier.weight(1f)) {
+                Row {
+                    TextField(value = str1 , onValueChange = input1 )
+                }
+                Row {
+                    Image(painter = painterResource(), contentDescription = )
+                    TextField(value = str2 , onValueChange = input2 )
+                }
+            }
 
+        }
     }
 }
